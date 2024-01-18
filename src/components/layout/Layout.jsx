4ccd5e57f85bd65a69096164c9936749e5a5d7b3
Iryna Artymuk <artimyk@ukr.net>
2024-01-18 11:39:21 +0200

    feat(app): fix header import

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import Header from '@/components/main/Header/Header';
+import Header from '@/components/main/header/Header';
 import Footer from '@/components/main/Footer/Footer';
 
 const Layout = () => {
