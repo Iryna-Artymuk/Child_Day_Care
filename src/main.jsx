3f7a50f12a3900af766cd50de314e517bf95beea
Iryna Artymuk <artimyk@ukr.net>
2024-01-20 15:47:46 +0200

    feat(add font import): add font import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import '@/index.scss';
-
+import './fonts/Comic_Sans_MS_Bold.ttf';
+import './fonts/Comic_Sans_MS.ttf';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
