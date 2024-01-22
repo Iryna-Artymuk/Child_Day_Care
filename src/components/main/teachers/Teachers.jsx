cdb65414a51be17d50937f7dbfa27183777e41c5
Iryna Artymuk <artimyk@ukr.net>
2024-01-22 16:34:38 +0200

    feat(teacher): add teacher img

--- src/components/main/teachers/Teachers.jsx
+++ src/components/main/teachers/Teachers.jsx
@@ -1,12 +1,36 @@
 import Container from '@/components/Container/Container';
 import styles from './Teachers.module.scss';
-
+import img1 from '@/assets/images/teachers/img1.png';
+import img2 from '@/assets/images/teachers/img2.png';
+import img3 from '@/assets/images/teachers/img3.png';
 const Teachers = () => {
   return (
     <section id="teachers" className={styles.teachers}>
       <Container>
         <div className="contentWrapper">
           <h2 className="title"> Наші вихователі</h2>
+          <div>
+            <ul className={styles.teachersList}>
+              <li className={styles.teachersListItem}>
+                <div className={styles.info}>
+                  <img src={img1} alt="" />
+                  <p>Келеберда Світлана Миколаївна</p>
+                </div>
+              </li>
+              <li className={styles.teachersListItem}>
+                <div className={styles.info}>
+                  <img src={img2} alt="" />
+                  <p>Чурута Ірина Григорівна </p>
+                </div>
+              </li>
+              <li className={styles.teachersListItem}>
+                <div className={styles.info}>
+                  <img src={img3} alt="" />
+                  <p>Наталія Миколаівна</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </Container>
     </section>
