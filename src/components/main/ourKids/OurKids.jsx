9c5298b54809a43ac61828e4bcffb27ee8a8fcaa
Iryna Artymuk <artimyk@ukr.net>
2024-01-25 10:38:40 +0200

    feat(our kids): add search icon

--- src/components/main/ourKids/OurKids.jsx
+++ src/components/main/ourKids/OurKids.jsx
@@ -5,6 +5,8 @@ import styles from './OurKids.module.scss';
 import { kidsData } from '@/constants/data/kidsData';
 
 import Card from './Card';
+
+import sprite from '@/assets/icons/sprite.svg';
 import { useEffect, useState } from 'react';
 const OurKids = () => {
   const [searchInputValue, setsearchInputValue] = useState('');
@@ -20,20 +22,27 @@ const OurKids = () => {
     setFilterKidsData(filterKids);
   }, [searchInputValue]);
   return (
-    <section id="ourKids" className={styles.ourKids}>
+    <section id="ourKids">
       <Container>
         <div className="contentWrapper">
-          <h2 className="title">Наші діти</h2>
-          <input
-            className={styles.searchInput}
-            type="text"
-            value={searchInputValue}
-            onChange={handelChange}
-            placeholder="введіть ім'я"
-            title="пошук по імені або прізвищу"
-          />
-          <ul className={ styles.kidsList }>
-            <li></li>
+          <div className={styles.searchWrapper}>
+            <h2 className="title">Наші діти</h2>
+            <label>
+              <svg className={styles.icon1} width={20} height={20}>
+                <use href={`${sprite}#${'icon-search'}`} />
+              </svg>
+              <input
+                className={styles.searchInput}
+                type="text"
+                value={searchInputValue}
+                onChange={handelChange}
+                placeholder="введіть ім'я"
+                title="пошук по імені або прізвищу"
+              />
+            </label>
+          </div>
+
+          <ul className={styles.kidsList}>
             {filterKidsData.map(child => {
               return (
                 <li key={child.name}>
